aeb81b15bab4c59ea28b15bffbf0c88be0be4252
Akira Chen <man4781747@gmail.com>
2022-01-27 02:41:56 +0000

    first commit

---
+++ PythonFlaskServer/BuildImage.sh
@@ -0,0 +1,1 @@
+docker build -t ts-flask-image:latest . --no-cache
---
+++ PythonFlaskServer/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+import json
+import datetime
+import os
+
+import app.loginSys as loginSys
+import app.TreeStudio as loginTS
+
+app = Flask(__name__,static_url_path='/static', static_folder=os.path.join(os.path.split(os.path.realpath(__file__))[0],'static'))
+
+loginSys.load_app(app)
+loginTS.load_app(app)
+
+@app.route('/test')
+def index():
+    return render_template("/home/man4781747/project/VueCliTest/Test/index.html")
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=9000)
